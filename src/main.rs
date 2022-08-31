fb2624d10261ef73462af58d8938f81321581935
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-09-01 01:25:47 +0200

    Add let_chains back to appease whatever weird nightly we're getting on CI
    This is truly strange.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! SPC700 assembler.
 
-#![feature(test, result_flattening, is_some_with, get_mut_unchecked, iterator_try_collect, if_let_guard)]
+#![feature(test, result_flattening, is_some_with, get_mut_unchecked, iterator_try_collect, if_let_guard, let_chains)]
 #![deny(clippy::all, clippy::pedantic, clippy::nursery)]
 #![deny(missing_docs)]
 

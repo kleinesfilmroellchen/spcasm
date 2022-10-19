9a8a1cff6ca19e5dc470df126ff094abe0b78d54
kleines Filmröllchen <filmroellchen@serenityos.org>
2022-10-19 11:12:12 +0200

    Web: Clean the DOM if the user copy-pastes divs
    This leads to ugly input fields.

--- spcasm-web/www/index.js
+++ spcasm-web/www/index.js
@@ -1,7 +1,14 @@
 import * as spcasm from "spcasm-web-wasm";
 
-document
-  .querySelector("code.assembly-source")
-  .addEventListener("input", spcasm.on_assembly_change, false);
+function cleanDOMStructure(event) {
+  const target = event.target;
+  const text = target.innerText;
+  // If there are div child elements, user most likely copy-pasted and we need to clean the DOM.
+  if (target.querySelectorAll("div").length > 0) target.innerHTML = text;
+}
+
+const sourceInput = document.querySelector("code.assembly-source");
+sourceInput.addEventListener("input", spcasm.on_assembly_change, false);
+sourceInput.addEventListener("input", cleanDOMStructure, false);
 
 spcasm.on_assembly_change();
